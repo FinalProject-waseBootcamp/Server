1e280e8c116204211097164a8415b76990d7f7b1
Ester-Tofik <325202349@mby.co.il>
2022-08-04 12:57:40 +0300

    fix UserSchema

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from './dto/user.dto';
+import { User } from '../dto/user.dto';
 
 @Injectable()
 export class UserService {
