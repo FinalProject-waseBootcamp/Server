a303bc53fa8290408ca25326a988dce40b68c90a
OraBenesh <orabenesh@gmail.com>
2022-08-08 13:35:11 +0300

    fix system details

--- src/dto/system.dto.ts
+++ src/dto/system.dto.ts
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 
 export interface System extends mongoose.Document {
-  name: string;
-  description: string;
+  uid: string;
   adminId: string;
+  name: string;
+  topic: string;
+  description: string;
+  communicationDetails: string;
 }
--- src/schema/system.schema.ts
+++ src/schema/system.schema.ts
@@ -7,14 +7,24 @@ export class System {
   constructor() {
     console.log('system construct');
   }
+
+  @Prop({ required: true })
+  uid: string;
+
   @Prop({ required: true })
   name: string;
+
+  @Prop({ required: true })
+  topic: string;
 
   @Prop()
   description: string;
 
   @Prop({ required: true })
   adminId: string;
+
+  @Prop({ required: true })
+  communicationDetails: string;
 }
 
 export const SystemSchema = SchemaFactory.createForClass(System);
