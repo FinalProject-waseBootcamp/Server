cc38b5bf95302c3079c7644cfcb4fdf7537e7250
Ester-Tofik <325202349@mby.co.il>
2022-09-02 01:48:01 +0300

    start tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule, ConfigService } from '@nestjs/config'
-import { NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
-import { AuthMiddleware } from './auth/auth.middleware';
 import { SystemModule } from './system/system.module';
 import { MarkerModule } from './marker/marker.module';
 
---
+++ src/marker/marker.controller.spec.ts
@@ -0,0 +1,27 @@
+import { MarkerController } from './marker.controller';
+import { MarkerService } from './marker.service';
+import { Test } from '@nestjs/testing';
+
+
+describe('MarkerController', () => {
+  let markerController: MarkerController;
+  let markerService: MarkerService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+        controllers: [MarkerController],
+        providers: [MarkerService],
+      }).compile();
+
+      markerService = moduleRef.get<MarkerService>(MarkerService);
+      markerController = moduleRef.get<MarkerController>(MarkerController);
+  });
+
+  describe('findAll', () => {
+    it('should return an array of cats', async () => {
+      const result = ['test'];
+      jest.spyOn(markerService, 'findAll').mockImplementation(() => result);
+      expect(await markerController.getAllMarkers()).toBe(result);
+    });
+  });
+});
