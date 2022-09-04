3d9ce3c9220b44d65978431456a9d65a6c91c9c1
OraBenesh <orabenesh@gmail.com>
2022-09-04 20:37:55 +0300

    connect server to mongo atlas cloud

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { ManagersModule } from './managers/managers.module';
 @Module({
   imports: [
     // MongooseModule.forRoot('mongodb://localhost:27017/nest-db'),
-    MongooseModule.forRoot('mongodb+srv://E_O:rTfFpwUcIASxk0dJ@cluster0.lw3xzo6.mongodb.net/?retryWrites=true&w=majority'),
+    MongooseModule.forRoot('mongodb+srv://E_O:2bdnEKR0dY7sPZLd@cluster0.lw3xzo6.mongodb.net/?retryWrites=true&w=majority'),
     // ConfigModule.load(path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
     // AuthModule,
     UserModule,
