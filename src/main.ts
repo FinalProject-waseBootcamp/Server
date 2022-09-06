732427db54655cb113da67427740b629b934c80a
OraBenesh <orabenesh@gmail.com>
2022-09-06 10:30:02 +0300

    slash config

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // import { initializeApp } from 'firebase-admin/app';
 import { Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 import * as admin from 'firebase-admin';
 import { ServiceAccount } from 'firebase-admin';
-import { ConfigModule } from '@nestjs/config';
-ConfigModule.forRoot();
+// import { ConfigModule } from '@nestjs/config';
+// ConfigModule.forRoot();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
